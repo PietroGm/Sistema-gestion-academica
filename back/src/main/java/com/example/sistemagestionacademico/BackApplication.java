6099636307fe6435caf1b4831c152c3b6859c59a
PietroGm <u17211760@utp.edu.pe>
2023-09-05 02:12:45 -0500

    creacion1

---
+++ back/src/main/java/com/example/sistemagestionacademico/BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemagestionacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionAcademicoApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/sistemagestionacademico/SistemaGestionAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemagestionacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
